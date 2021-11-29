e7d9ea72de831acd8e02bef392fc78848d055987
Le-M1nh <hoangminh8162@gmail.com>
2021-11-30 00:43:42 +0700

    select class now sync with db

--- Server/public/js/index.js
+++ Server/public/js/index.js
@@ -1,19 +1,19 @@
-
 const attendancesTable = document.querySelector("table")
-const classSelect = document.getElementById("select_classes")
+const select = document.getElementById("select")
 
 console.log("index.js")
 
-classSelect.addEventListener("change", function (err) {
+select.addEventListener("change", function (err) {
   fetch("/attendances").then(function (response) {
     response.json().then(function (data) {
       removeRows()
-      
+
+      console.log(select.value)
       if (data.length > 0) {
         for (var i = 0; i < data.length; i++) {
           const attendance = data[i]
 
-          if (attendance.class_id == classSelect.value) {
+          if (attendance.class_id == select.value) {
             const newRow =attendancesTable.insertRow(-1)
           
             indexCell = newRow.insertCell(0)
@@ -41,3 +41,21 @@ function removeRows () {
     attendancesTable.deleteRow(1)
   }
 }
+
+fetch("/classes").then(function (response) {
+  response.json().then(function (data) {
+
+    if (data.length > 0) {
+
+      for (var i = 0; i < data.length; i++) {
+        const Class = data[i];
+
+        const option = document.createElement("option")
+        option.value = Class.id
+        option.text = Class.id + " " + Class.subject
+        select.add(option, select[i])
+      }
+    } else {
+    }
+  })
+})
